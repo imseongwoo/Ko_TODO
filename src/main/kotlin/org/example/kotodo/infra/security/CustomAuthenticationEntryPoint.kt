3a19bc7057d0e3756a134f420c9f988e6ae8d2ff
임성우 <seongwoorang@naver.com>
2024-06-07 15:39:24 +0900

    Add 401 exception handling

---
+++ src/main/kotlin/org/example/kotodo/infra/security/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,29 @@
+package org.example.kotodo.infra.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.example.kotodo.domain.common.exception.dto.ErrorDTO
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import org.springframework.web.ErrorResponse
+
+@Component
+class CustomAuthenticationEntrypoint: AuthenticationEntryPoint {
+
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+
+        val objectMapper = ObjectMapper()
+        val jsonString = objectMapper.writeValueAsString(ErrorDTO("JWT verification failed"))
+        response.writer.write(jsonString)
+    }
+}
--- src/main/kotlin/org/example/kotodo/infra/security/SecurityConfig.kt
+++ src/main/kotlin/org/example/kotodo/infra/security/SecurityConfig.kt
@@ -3,15 +3,19 @@ package org.example.kotodo.infra.security
 import org.example.kotodo.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntrypoint: AuthenticationEntryPoint
 ) {
 
     @Bean
@@ -30,6 +34,9 @@ class SecurityConfig(
                     .anyRequest().authenticated()
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling{
+                it.authenticationEntryPoint(authenticationEntrypoint)
+            }
             .build()
     }
 }
